f49acf144803901dbe9a2111be200e6a79f92a2a
Vitalij Mast <vitali_mast@gmx.net>
2015-08-28 13:42:45 +0200

    implement director, file browser and image preview handler

--- SymbolScanner/WinSymbolScanner.h
+++ SymbolScanner/WinSymbolScanner.h
@@ -4,16 +4,32 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_WinSymbolScanner.h"
 
+class QFileSystemModel;
+class QGraphicsScene;
+
 class WinSymbolScanner : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    WinSymbolScanner(QWidget *parent = 0);
-    ~WinSymbolScanner();
+  WinSymbolScanner(QWidget *parent = 0);
+  ~WinSymbolScanner();
+
+protected:
+  bool eventFilter(QObject* object, QEvent* event) override;
+  void resizeEvent(QResizeEvent* event) override;
+
+public slots:
+  void onc_treeViewSelectionModel_currentChanged(const QModelIndex& current, const QModelIndex& previous);
+  void onc_listViewSelectionModel_currentChanged(const QModelIndex& current, const QModelIndex& previous);
+
+  void setSelectedFile(const QString& fileName = QString());
 
 private:
-    Ui::WinSymbolScannerClass ui;
+  Ui::WinSymbolScannerClass ui;
+  QFileSystemModel *dirModel;
+  QFileSystemModel *fileModel;
+  QGraphicsScene *previewScene;
 };
 
 #endif // WINSYMBOLSCANNER_H
