b41a897525a3b3706357c0b776c543af38020dc9
Vitalij Mast <vitali_mast@gmx.net>
2015-09-12 14:59:18 +0200

    grid filter: add slots

--- SymbolScanner/WinImageFilterGrid.cpp
+++ SymbolScanner/WinImageFilterGrid.cpp
@@ -8,7 +8,7 @@ WinImageFilterGrid::WinImageFilterGrid(QWidget *parent)
   : QMainWindowChild(parent)
 {
   _ui.setupUi(this);
-
+  
   // setup file listener
   _fileModel = new QFileSystemModel(this);
   _fileModel->setFilter(QDir::Files);
@@ -26,92 +26,55 @@ WinImageFilterGrid::~WinImageFilterGrid()
 
 }
 
+void WinImageFilterGrid::refreshPreview(void)
+{
+  // _currentFileName, _autoRotate, _filterPreviewType, _filterUpperColor, _filterLowerColor
+
+  /*
+  QPixmap orgPix = parentMainWindow()->imageCache().value(fileName);
+  cv::Mat org = qPixmapToCvMat(orgPix, false);
+
+  cv::Mat greyMat;
+  cv::cvtColor(org, greyMat, CV_BGR2GRAY);
+
+  QPixmap editedPix = cvMatToQPixmap(greyMat);
+  _ui.labelPreview->setPixmap(editedPix);
+  */
+}
+
+void WinImageFilterGrid::on_checkBoxAutoRotate_stateChanged(int state)
+{
+  _autoRotate = (state == Qt::Checked);
+  refreshPreview();
+}
+
+void WinImageFilterGrid::on_buttonGroupPreviewSelection_buttonClicked(QAbstractButton* button)
+{
+  _filterPreviewType = button->property("id").toInt();
+  refreshPreview();
+}
+
+void WinImageFilterGrid::on_widgetUpperColorSelector_colorChanged(QColor color)
+{
+  _filterUpperColor = color;
+  refreshPreview();
+}
+
+void WinImageFilterGrid::on_widgetLowerColorSelector_colorChanged(QColor color)
+{
+  _filterLowerColor = color;
+  refreshPreview();
+}
+
+void WinImageFilterGrid::onc_listViewSelectionModel_currentChanged(const QModelIndex& current, const QModelIndex& previous)
+{
+  _currentFileName = _fileModel->filePath(current);
+  refreshPreview();
+}
+
 void WinImageFilterGrid::setCurrentFolder(const QString& directory)
 {
   _fileModel->setNameFilters(parentMainWindow()->property("defaultFileFilter").toStringList());
   _ui.listViewFiles->clearSelection();
   _ui.listViewFiles->setRootIndex(_fileModel->setRootPath(directory));
 }
-
-void WinImageFilterGrid::onc_listViewSelectionModel_currentChanged(const QModelIndex& current, const QModelIndex& previous)
-{
-  auto fileName = _fileModel->filePath(current);
-
-  QPixmap orgPix = parentMainWindow()->imageCache().value(fileName);
-  cv::Mat org = qPixmapToCvMat(orgPix, false);
-
-  //cv::Mat greyMat;
-  //cv::cvtColor(org, greyMat, CV_BGR2GRAY);
-
-  QPixmap editedPix = cvMatToQPixmap(greyMat);
-  _ui.labelPreview->setPixmap(editedPix);
-}
-
-/*
-void WinImageFilterGrid::refreshPreviewImage(void)
-{
-  if (_ui.buttonGroup->checkedButton() == _ui.radioButtonRaster)
-  {
-    cv::Scalar low = qColorToCvScalar(_ui.colorLineEditRasterLower->color());
-    cv::Scalar up = qColorToCvScalar(_ui.colorLineEditRasterUpper->color());
-
-    cv::cvtColor(imMatOrignal, imMatPreview, cv::COLOR_BGR2HSV);
-    cv::inRange(imMatPreview, low, up, imMatPreview);
-  } else if (_ui.buttonGroup->checkedButton() == _ui.radioButtonSign)
-  {
-    cv::Scalar low = qColorToCvScalar(_ui.colorLineEditSignsLower->color());
-    cv::Scalar up = qColorToCvScalar(_ui.colorLineEditSignsUpper->color());
-
-    cv::cvtColor(imMatOrignal, imMatPreview, cv::COLOR_BGR2HSV);
-    cv::inRange(imMatPreview, low, up, imMatPreview);
-  } else
-  {
-    imMatOrignal.copyTo(imMatPreview);
-    //cv::cvtColor(imMatOrignal, imMatPreview, cv::COLOR_BGR2RGB);
-  }
-
-  _ui.labelImage->setPixmap(cvMatToQPixmap(imMatPreview));
-}
-
-void WinImageFilterGrid::on_widgetLowerFilter_colorChanged(QColor color)
-{
-  if (_ui.buttonGroup->checkedButton() == _ui.radioButtonRaster)
-    _ui.colorLineEditRasterLower->setColor(color);
-  else if (_ui.buttonGroup->checkedButton() == _ui.radioButtonSign)
-    _ui.colorLineEditSignsLower->setColor(color);
-  refreshPreviewImage();
-}
-
-void WinImageFilterGrid::on_widgetUpperFilter_colorChanged(QColor color)
-{
-  if (_ui.buttonGroup->checkedButton() == _ui.radioButtonRaster)
-    _ui.colorLineEditRasterUpper->setColor(color);
-  else if (_ui.buttonGroup->checkedButton() == _ui.radioButtonSign)
-    _ui.colorLineEditSignsUpper->setColor(color);
-  refreshPreviewImage();
-}
-
-void WinImageFilterGrid::on_buttonGroup_buttonClicked(QAbstractButton * button)
-{
-  auto enFilter = button != _ui.radioButtonOriginal;
-  _ui.groupBoxLower->setEnabled(enFilter);
-  _ui.groupBoxUpper->setEnabled(enFilter);
-
-  if (button == _ui.radioButtonRaster)
-  {
-    _ui.widgetLowerFilter->setColor(_ui.colorLineEditRasterLower->color());
-    _ui.widgetUpperFilter->setColor(_ui.colorLineEditRasterUpper->color());
-  } else if (_ui.buttonGroup->checkedButton() == _ui.radioButtonSign)
-  {
-    _ui.widgetLowerFilter->setColor(_ui.colorLineEditSignsLower->color());
-    _ui.widgetUpperFilter->setColor(_ui.colorLineEditSignsUpper->color());
-  } else
-  {
-    QColor c = Qt::white;
-    _ui.widgetLowerFilter->setColor(c);
-    _ui.widgetUpperFilter->setColor(c);
-  }
-  refreshPreviewImage();
-}
-
-*/
--- SymbolScanner/WinImageFilterGrid.h
+++ SymbolScanner/WinImageFilterGrid.h
@@ -17,15 +17,26 @@ public:
   WinImageFilterGrid(QWidget *parent = 0);
   ~WinImageFilterGrid();
 
-  void refreshPreviewImage(void);
-
   public slots:
+  void refreshPreview(void);
+
+  void on_checkBoxAutoRotate_stateChanged(int state);
+  void on_buttonGroupPreviewSelection_buttonClicked(QAbstractButton* button);
+  void on_widgetUpperColorSelector_colorChanged(QColor color);
+  void on_widgetLowerColorSelector_colorChanged(QColor color);
+
   void onc_listViewSelectionModel_currentChanged(const QModelIndex& current, const QModelIndex& previous);
   void setCurrentFolder(const QString& directory);
 
 private:
   Ui::WinImageFilterGrid _ui;
   QFileSystemModel *_fileModel;
+  
+  bool _autoRotate;
+  QString _currentFileName;
+  int _filterPreviewType;
+  QColor _filterUpperColor;
+  QColor _filterLowerColor;
 };
 
 #endif // WINIMAGEFILTEROPTIONS_H
